a2da08565ef7a15ee20bf5915365a188a85b64f8
Abram Stamper <abram.j.stamper@me.com>
2016-04-23 19:34:24 -0500

    Modified main to retrieve data from server for vendors

--- main.py
+++ main.py
@@ -14,52 +14,37 @@ app.config['MYSQL_DB'] = 'event'
 app.config['SECRET_KEY'] = 'test'
 mysql.init_app(app)
 
+## API
 
-# @app.route('/')
-# def users():
-#     cur = mysql.connection.cursor()
-#     cur.execute('SELECT * FROM event.user')
-#     rv = cur.fetchall()
-#     return render_template('users.html', rv=rv)
+##
+## Budget
+##
 
-@app.route('/event/<int:event_id>/task')
-def task(event_id):
-    newEvent = Event.loadEvent(event_id)
-    response = newEvent.getTasksForEvent();
-    return render_template('task/task.html', response=response)
-
-@app.route('/event/<int:event_id>/task/new')
-def newTask(event_id):
-    newEvent = Event.loadEvent(event_id)
-    form = NewTask(request.form)
-    response = newEvent.getTasksForEvent();
-    return render_template('task/new.html', response=response)
-
+# returns budget for a given event_id
 @app.route('/event/<int:event_id>/budget')
 def budget(event_id):
-    newEvent = Event.loadEvent(event_id)
-    response = newEvent.getTasksForEvent();
+    current_event = Event.loadEvent(event_id)
+    #need to code get budget
+    response = current_event.getTasksForEvent();
     return render_template('budget/budget.html', response=response)
 
-@app.route('/event/<int:event_id>/ticket')
-def ticket(event_id):
-    newEvent = Event.loadEvent(event_id)
-    response = newEvent.getTasksForEvent();
-    return render_template('ticket/ticket.html', response=response)
+##
+## Event
+##
 
-
+# returns all events from the database - not user specific (yet)
 @app.route('/event')
 def event():
     response = Event.getAllEvents()
     return render_template('event/event.html', response=response)
 
-
+# returns a single event page for a given event_id
 @app.route('/event/<int:event_id>')
 def event_show(event_id):
     response = Event.loadEvent(event_id)
     return render_template('event/show.html', response=response)
 
-
+# creates a new event
 @app.route('/event/new', methods=['GET', 'POST'])
 def newEvent():
     form = NewEvent(request.form)
@@ -70,8 +55,50 @@ def newEvent():
 
     return render_template('event/new.html', form=form)
 
+##
+## Task
+##
 
-# an instance of an event - basically a model of an event in the database
+# returns the tasks for a given event_id
+@app.route('/event/<int:event_id>/task')
+def task(event_id):
+    current_event = Event.loadEvent(event_id)
+    response = current_event.getTasksForEvent();
+    return render_template('task/task.html', response=response)
+
+# creates a new task fo a given event_id
+@app.route('/event/<int:event_id>/task/new')
+def newTask(event_id):
+    current_event = Event.loadEvent(event_id)
+    form = NewTask(request.form)
+    response = current_event.getTasksForEvent();
+    return render_template('task/new.html', response=response)
+
+##
+## Ticket
+##
+
+# returns tickets for a given event_id
+@app.route('/event/<int:event_id>/ticket')
+def ticket(event_id):
+    current_event = Event.loadEvent(event_id)
+    response = current_event.getTasksForEvent();
+    return render_template('ticket/ticket.html', response=response)
+
+##
+## Vendor
+##
+
+# returns all vendors from the database
+@app.route('/vendor')
+def vendor():
+    response = Vendor.getAllVendors()
+    return render_template('vendor/vendor.html', response=response)
+
+##
+## Models
+##
+# an instance of an event
 class Event():
     id = -1
     name = ""
@@ -91,6 +118,7 @@ class Event():
         self.setupStart = setupStart
         self.teardownEnd = teardownEnd
 
+    # retrieves the tasks for a given event instance and puts them in a dictionary
     def getTasksForEvent(self):
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT event.id, task.id, task.priority, task.name, task.dateDue, task.status, task.assignedTo FROM event.task JOIN event.event ON event_id = event.id WHERE event_id = %(id)s;', {'id' : self.id})
@@ -102,6 +130,7 @@ class Event():
             return [{'event_id' : self.id}]
         return allTasks
 
+    # creates a task for a given event instance - To be implemented
     def createTaskForEvent(self, id, name, dueDate, priority, status):
         #hardcoded params - user_assign - admin@admin.com
         params = {"id": id, "name": name, "dueDate": dueDate, "priority": priority, "status": status,
@@ -147,6 +176,32 @@ class Event():
         teardownEnd = data[6]
         # print(id, name, date_start, date_end, description, setupStart, teardownEnd);
         return cls(id, name, date_start, date_end, description, setupStart, teardownEnd);
+
+# an instance of a vendor
+class Vendor:
+    id = -1
+    name = ""
+    phone = ""
+    address = ""
+    email = ""
+    #array of invoices - to be implemented
+    invoices = []
+
+    def __init__(self, id, name, phone, address, email):
+        self.id = id
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.email = email
+
+    # return all vendors
+    @staticmethod
+    def getAllVendors():
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM event.vendor;')
+        data = cursor.fetchall()
+        return data
+
 
 
 if __name__ == '__main__':
